0ceba6016317009a9bb559b901f20afbcef14b6d
IzaqueSA <izaquesousadeandrade@gmail.com>
2026-01-11 21:29:51 +0000

    Primeiro commit com os arquivos prontos

---
+++ AnonimeitosSetup/AnonimeitosSetup/src/main/java/com/anonimeitos/anonimeitosSetup/AnonimeitosSetupApplication.java
@@ -0,0 +1,13 @@
+package com.anonimeitos.anonimeitosSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonimeitosSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonimeitosSetupApplication.class, args);
+	}
+
+}
---
+++ AnonimeitosSetup/AnonimeitosSetup/src/test/java/com/anonimeitos/interstrakt/InterstraktSetup/InterstraktSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonimeitos.interstrakt.InterstraktSetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterstraktSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
